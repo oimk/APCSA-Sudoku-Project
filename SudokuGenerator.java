5b0a0087ede97d0dc906e66850995147a76a1191
Gurgle <181671980+oimk@users.noreply.github.com>
2026-04-05 19:14:19 +0000

    Random Sudoku Generator complete!

--- SudokuGenerator.java
+++ SudokuGenerator.java
@@ -1,13 +1,17 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class SudokuGenerator {
     private static int[][] board = new int[9][9];
     
     
     public static void main(String[] args){
-        fillDiagonal(0,0);
-        fillDiagonal(3,3);
+        fillDiagonal(0, 0);
+        fillDiagonal(3, 3);
         fillDiagonal(6,6);
+        fillGrid(0,3);
+        printBoard();
 
     }
 
@@ -24,80 +28,65 @@ public class SudokuGenerator {
             }
         }
     }
+    
 
-    public static void fillGrid(int row, int col){
-        if (col == 9){
-            return;
-        }
-
+    public static boolean fillGrid(int row, int col){
         if (row == 9){
-            fillGrid(0, col+1);
+            return true;
         }
 
-        if (board[row][col] == 0){
-            fillGrid(row+1, col);
-        } else {
-
+        if (col == 9){
+            return fillGrid(row+1, 0);
         }
+        if (board[row][col] != 0){
+            return fillGrid(row, col+1);
+        }
+
+        Integer[] list = {1,2,3,4,5,6,7,8,9};
+        ArrayList<Integer> solutions = new ArrayList<>(Arrays.asList(list));
+        Collections.shuffle(solutions);
+
+        for (int n : solutions){
+            if(isVaild(row, col, n)){
+                board[row][col] = n;
+                if (fillGrid(row, col+1)){
+                    return true;
+                }
+            }
+            board[row][col] = 0;
+        }
+        return false;
     }
 
-    public static void fillElement(int row, int col){
-        board[row][col] = findValid(row, col);
-    }
 
-    //outputs a vaild number
-    public static int findValid(int row, int col){
-        ArrayList<Integer> buffer = new ArrayList<>();
+    public static boolean isVaild(int row, int col, int val){
+        //check col
+        for (int i=0; i<board[0].length; i++){
+            int num = board[row][i];
+            if (num == val){
+                return false;
+            }
+        }
 
         //check row
-        for (int i=0; i<board.length; i++){
-            int num = board[i][col];
-            if (num != 0){
-                buffer.add(num);
+        for (int j=0; j<board.length; j++){
+            int num = board[j][col];
+            if (num == val){
+                return false;
             }
         }
 
-        //if all rows are filled, try a different number in the p
-        if (buffer.size() == 9){
-            
-        }
+        int rowOrigin = (int)(row/3)*3;
+        int colOrigin = (int)(col/3)*3;
 
-        //check col
-        for (int j=0; j<board[0].length; j++){
-            int num = board[row][j];
-            if (num != 0 && num != col && !buffer.contains(num)){
-                buffer.add(num);
-            }
-        }
-
-        if (buffer.size() == 9){
-            return -1;
-        }
-
-        // check grid
-        int rowOrgin = (int)(row/3) * 3;
-        int colOrgin = (int)(col/3) * 3;
-        
-        for (int r=rowOrgin; r<rowOrgin+3; r++){
-            if (r==row){
-                continue;
-            } else {
-                for (int c=colOrgin; c<colOrgin+3; c++){
-                    if (c==col){
-                        continue;
-                    } else{
-                        int num = board[r][c];
-                        if (num != 0 && !buffer.contains(num)){
-                            buffer.add(num);
-                        }
-                    }
+        for (int r=rowOrigin; r<rowOrigin+3; r++){
+            for (int c=colOrigin; c<colOrigin+3; c++){
+                if (board[r][c] == val){
+                    return false;
                 }
             }
         }
-
-
-
-
+        return true;
     }
 
 
